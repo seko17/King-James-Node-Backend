8773fd7e2ff3f86149fdcb477db7fe0da1b9c345
seko17 <34337236+seko17@users.noreply.github.com>
2023-08-30 20:40:16 +0200

    init

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const { FieldValue } = require('firebase-admin/firestore')
+const app = express()
+const port = 8383
+const { db } = require('./firebase-admin.js')
+
+app.use(express.json())
+
+//routes
+app.post('/addBrand', async (req, res) => {
+    const { name, url } = req.body
+    const brandsRef = db.collection('brands').doc('images')
+    const res2 = await brandsRef.set({
+        brandName: name,
+        imgUrl: url
+    }, { merge: true })
+ 
+    res.status(200).send(res2)
+})
+
+app.get('/data', async (req, res) => {
+    const peopleRef = db.collection('brands').doc('images')
+    const doc = await peopleRef.get()
+    if (!doc.exists) {
+        return res.sendStatus(400)
+    }
+
+    res.status(200).send(doc.data())
+})
+
+
+app.listen(port, () => console.log(`Server has started on port: ${port}`))
