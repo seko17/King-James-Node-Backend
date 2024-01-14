e5a49b378fd6cef5b851925dbb5982489cde69f9
seko17 <34337236+seko17@users.noreply.github.com>
2024-01-14 19:05:26 +0200

    add cors

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ require('dotenv').config();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET','POST','DELETE','PUT');
-    next();
-})
+app.use(cors());
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET','POST','DELETE','PUT');
+//     next();
+// })
 
 
 //routes
